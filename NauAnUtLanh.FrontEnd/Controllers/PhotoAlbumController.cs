7a3f3fa0d26817c607836f149ee2dcda0dccc1e2
Chau Nguyen <chau.nguyendong@ndc.net.vn>
2017-04-26 19:31:04 +0700

    2017-04-26-19-31

--- NauAnUtLanh.FrontEnd/Controllers/PhotoAlbumController.cs
+++ NauAnUtLanh.FrontEnd/Controllers/PhotoAlbumController.cs
@@ -15,8 +15,7 @@ namespace NauAnUtLanh.FrontEnd.Controllers
     public class PhotoAlbumController : Controller
     {
         private readonly NauAnUtLanhDbContext _db = new NauAnUtLanhDbContext();
-        private const int PageSize = 30;
-        private const string Folder = "~/upload/album";
+        private const int PageSize = 10;
 
         public async Task<ActionResult> Index(int? page)
         {
@@ -24,32 +23,6 @@ namespace NauAnUtLanh.FrontEnd.Controllers
             var albums =
                 await _db.PhotoAlbums.OrderByDescending(x => x.CreatedTime).Where(x => x.Activated).ToListAsync();
             return View(albums.ToPagedList(pageNumber , PageSize));
-        }
-
-        public async Task<ActionResult> Details(Guid? id)
-        {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var album = await _db.PhotoAlbums.FindAsync(id);
-            if (album == null) return HttpNotFound();
-            return View(album);
-        }
-
-        public ActionResult PhotosByAlbum(Guid id)
-        {
-            var folder = Server.MapPath($"{Folder}/{id}");
-            var files = Directory.GetFiles(folder).ToList();
-            var imgNameList = new List<string>();
-            foreach (var file in files)
-            {
-                var imgName = new FileInfo(file).Name;
-                imgNameList.Add(imgName);
-            }
-            var imgGallery = new ImageGallery
-            {
-                FolderName = $"{id}",
-                ImageNameList = imgNameList
-            };
-            return PartialView("_PhotoGallery", imgGallery);
         }
 
         protected override void Dispose(bool disposing)
