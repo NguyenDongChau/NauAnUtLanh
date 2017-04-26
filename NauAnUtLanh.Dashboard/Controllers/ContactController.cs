6820ff827d8defe0409b940622059487160dcafb
Chau Nguyen <chau.nguyendong@ndc.net.vn>
2017-04-26 16:00:44 +0700

    2017-04-26-16-00

---
+++ NauAnUtLanh.Dashboard/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NauAnUtLanh.Database;
+using PagedList;
+
+namespace NauAnUtLanh.Dashboard.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly NauAnUtLanhDbContext _db = new NauAnUtLanhDbContext();
+        private const int PageSize = 30;
+
+        public async Task<ActionResult> Index(int? page)
+        {
+            var pageNumber = page ?? 1;
+            var contacts = await _db.Contacts.OrderByDescending(x => x.CreatedTime).ToListAsync();
+            return View(contacts.ToPagedList(pageNumber, PageSize));
+        }
+
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if(id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var contact = await _db.Contacts.FindAsync(id);
+            if (contact == null) return HttpNotFound();
+            contact.Read = true;
+            _db.Entry(contact).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return View(contact);
+        }
+    }
+}
--- NauAnUtLanh.FrontEnd/App_Start/BundleConfig.cs
+++ NauAnUtLanh.FrontEnd/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace NauAnUtLanh.FrontEnd
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                    "~/Scripts/jquery-{version}.js"
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/jquery-ui-{version}.js"
                     ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -25,13 +26,19 @@ namespace NauAnUtLanh.FrontEnd
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                     "~/Scripts/bootstrap.js",
                     "~/Scripts/respond.js",
-                    "~/Scripts/lightbox.js"
+                    "~/Scripts/lightbox.js",
+                    "~/Scripts/pabulum/easing.js",
+                    "~/Scripts/pabulum/jquery.swipebox.js",
+                    "~/Scripts/pabulum/move-top.js",
+                    "~/Scripts/pabulum/responsiveslides.js"
                     ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                     "~/Content/bootstrap.css",
                     "~/Content/lightbox.css",
-                    "~/Content/site.css"
+                    //"~/Content/site.css",
+                    "~/Content/pabulum/style.css",
+                    "~/Content/pabulum/swipebox.css"
                     ));
         }
     }
